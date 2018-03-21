63f00432320d5ba06fc7db1010c35ec3baa6ebe3
xj <xujing9686@qq.com>
2018-03-21 11:25:11 +0800

    v1.2
    book success

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -12,9 +12,15 @@ return array(
 		'DB_CHARSET'=> 'utf8', // 字符集
 		'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
 		
+		//小程序APPID和APPSECRET
+		'APPID'=>'wx3e48bc922f5ecc42',
+		'APPSECRET'=>'4f339d067fb4bb1f4d5ed31a4b6de2c8',
 		
 		'BASE_URL' => 'http://localhost:8080/queueup',
 		
 		'ADMIN_USERNAME' => 'admin',
 		'ADMIN_PASSWORD' => '123456',
+		
+		'MODULE_ALLOW_LIST'    =>    array('Home','Api'),
+		'DEFAULT_MODULE'       =>    'Home',
 );
