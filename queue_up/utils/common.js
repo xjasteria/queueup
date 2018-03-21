8c09e37fbc4f935b7e84bfc8dc3600acff6b48ed
xj <xujing9686@qq.com>
2018-03-21 11:31:52 +0800

    weixin
    wei xin

---
+++ queue_up/utils/common.js
@@ -0,0 +1,6 @@
+var baseUrl = 'http://localhost:8080/queueup/';
+//var baseUrl = 'http://queueup.com:8080/';
+//var baseUrl = 'http://10.103.1.83:8080/queueup/';
+module.exports = {
+  baseUrl: baseUrl
+}
