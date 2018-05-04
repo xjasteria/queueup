fde5e742b39dc2a389ec7525b92e2a600af8f014
xj <xujing9686@qq.com>
2018-05-04 12:20:45 +0800

    base v2.0
    no new

---
+++ queue_up/utils/common.js
@@ -0,0 +1,6 @@
+var baseUrl = 'http://localhost:8080/queueup/';
+//var baseUrl = 'http://queueup.com:8080/';
+//var baseUrl = 'http://192.168.43.236:8080/queueup/';
+module.exports = {
+  baseUrl: baseUrl
+}
